b6617acb6200e2b889ab97702c696518ab2fffc8
gustavo867 <tartatavo3@gmail.com>
2021-04-13 19:04:15 -0300

    :alien: updating services

--- src/api/query.ts
+++ src/api/query.ts
@@ -5,3 +5,19 @@ export const fetchTodo = async () => {
 
   return res.data;
 };
+
+export const postTodo = async (data: { message: string; img: string[] }) => {
+  const formData = new FormData();
+  formData.append("message", data.message);
+  data.img.forEach((item) => {
+    formData.append("images", {
+      name: `file-${item}`,
+      type: "image/jpeg",
+      uri: item,
+    } as any);
+  });
+
+  const res = await api.post("todos", formData);
+
+  return res.data;
+};
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `http://192.168.1.14:3333/v1`,
+  baseURL: `http://192.168.1.14:3333/v2`,
 });
+
+export const BASE_URL = "http://192.168.1.14:3333";
 
 export default api;
